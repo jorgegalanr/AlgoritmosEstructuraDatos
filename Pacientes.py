c27fd9ac70660aabf17762314ba182d0ad9db9a4
JorgeG <jorgegalanrodriguez@gmail.com>
2024-01-07 16:51:30 +0100

    añadido clase SalaEmergencias con los métodos para la atención

--- Pacientes.py
+++ Pacientes.py
@@ -18,7 +18,38 @@ class Paciente:
     def __str__(self):
         return f"{self.Id_Paciente} - {self.nombre}: Urgencia {self.nivel_urgencia}, Espera {self.horas_espera} horas"
 
+# creamos un clase SalaEmergencias en la que gestionará el orden de prioridad
+class SalaEmergencias:
+    def __init__(self):
+        self.max_heap = MaxHeap()       # esta parte se define para la urgencia
+        self.min_heap = MinHeap()       # esta parte se define para los tiempos de espera
+    
+    # método para agregar a los pacientes que llegan
+    def add_paciente(self, paciente):
+        self.max_heap.push(paciente)
+        self.min_heap.push(paciente)
 
+    # método para ir sacando de la lista a los pacientes atendidos
+    def atender_paciente(self):
+        if not self.max_heap.is_empty() and self.max_heap.peek().nivel_urgencia == 10:
+            return self.max_heap.pop()
+        
+        if not self.min_heap.is_empty() and self.max_heap.peek().horas_espera > 5:
+            paciente_espera = self.min_heap.pop()
+            self.max_heap.remove(paciente_espera)
+            return paciente_espera
+        
+        return None
+    
+    # método para sacar el listado de pacientes ya atendidos
+    def reportes(self, num_pacientes):
+        reporte = []
+
+        for p in range(num_pacientes):
+            paciente = self.atender_paciente()
+            if paciente:
+                reporte.append(str(paciente))
+            return reporte
     
 
 
